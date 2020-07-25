cedbc3154d050fdce0e6d55da9da965a06b7cf9c
Tom Smeets <tom.tsmeets@gmail.com>
2020-07-25 17:18:42 +0200

    cargo fmt

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
 fn main() {
     let dkp_path = std::env::var("DEVKITPRO").expect("Please set $DEVKITPRO");
-    println!("cargo:rustc-link-search=native={}/devkitPPC/powerpc-eabi/lib", dkp_path);
+    println!(
+        "cargo:rustc-link-search=native={}/devkitPPC/powerpc-eabi/lib",
+        dkp_path
+    );
     println!("cargo:rustc-link-search=native={}/libogc/lib/wii", dkp_path);
     println!("cargo:rustc-link-lib=static=sysbase");
 }
